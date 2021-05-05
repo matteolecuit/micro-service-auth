3e5538412ce94f4b244e62b382592f6803a04d54
aelmaknati <abdellatif.elmaknati@dakilab.fr>
2021-05-05 07:00:48 +0200

    Init

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const crypto = require('crypto')
 const app = express()
 const port = 3000;
 
+
+// Fonction lancée eu démarrage du service
 const start = async () => {
 
     // Envoie d'une requete
@@ -13,28 +15,37 @@ const start = async () => {
     let config = {
         headers: {}
     }
-    let response = await axios.post("http://<url>", body, config);
+    let response;
+    try {
+        response = await axios.post("http://<host>:<port>", body, config);
+    }catch(e){
+        console.error(e.response ? e.response.data : e)
+        return
+    }
     console.log(response.data);
 
-    // Création d'un endpoint en GET
-    app.get('/url_du_endpoint_en get', async (req, res) => {
-        let headers = req.headers;
-        res.json()
-    })
-
-    // Création d'un endpoint en POST
-    app.post('/url_du_endpoint_en post', async (req, res) => {
-        let body = req.body;
-        let headers = req.headers;
-        res.json()
-    })
-
-    // Lancement du service
-    app.listen(port, () => {
-        console.log(`Service listening at http://localhost:${port}`)
-    })
-
 }
+
+// Création d'un endpoint en GET
+app.get('/url_du_endpoint_en_get', async (req, res) => {
+    let headers = req.headers;
+    res.json()
+})
+
+// Création d'un endpoint en POST
+app.post('/url_du_endpoint_en_post', async (req, res) => {
+    let body = req.body;
+    let headers = req.headers;
+    res.json()
+})
+
+
+// Lancement du service
+app.listen(port, () => {
+    console.log(`Service listening at http://localhost:${port}`)
+    start();
+})
+
 
 
 /**
@@ -48,8 +59,3 @@ function encrypt(secretKey , publicKey){
         .update(publicKey)
         .digest('hex');
 }
-
-
-
-start();
-
